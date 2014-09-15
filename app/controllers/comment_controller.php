23a04c7fff16a2afa9119a8395aa97f83bdbd8c5
Jeszy Tanada <m.tanada@klab.com>
2014-09-15 09:41:09 +0800

    Added new comment files

---
+++ app/controllers/comment_controller.php
@@ -0,0 +1,12 @@
+<?php
+class CommentController extends AppController
+{
+    /** 
+    *** -Displays specific Thread and its containing Comments
+    **/
+    public function view() {    
+        $thread = Thread::get(Param::get('thread_id'));
+        $comments = Comment::getComments($thread->id);
+        $this->set(get_defined_vars());
+    }
+}
